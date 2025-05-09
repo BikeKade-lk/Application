358f15310bb750c9a8b83705d4b4cdfb7d21cf7c
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2025-05-09 18:08:37 +0530

    Update Header.jsx

--- applicationfrontend/src/components/Header.jsx
+++ applicationfrontend/src/components/Header.jsx
@@ -1,34 +1,64 @@
 // src/components/Header.js
 import React from "react";
 import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
-import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+
+// MUI Icons
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+import LoginIcon from "@mui/icons-material/Login";
 
 export default function Header() {
   return (
     <AppBar position="static">
       <Toolbar>
-        {/* Left side: Logo and Title */}
+        {/* Left side: Logo and Title wrapped in Link */}
         <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
-          <img
-            src={logo}
-            alt="BikeKade.lk Logo"
-            style={{ height: "40px", marginRight: "10px" }}
-          />
-          <Typography variant="h6" component="div">
-            BikeKade.lk
-          </Typography>
+          <Link
+            to="/"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+              color: "inherit",
+            }}
+          >
+            <img
+              src={logo}
+              alt="BikeKade.lk Logo"
+              style={{ height: "40px", marginRight: "10px" }}
+            />
+            <Typography variant="h6" component="div">
+              BikeKade.lk
+            </Typography>
+          </Link>
         </Box>
 
-        {/* Right side: Home Button */}
+        {/* Right side: Navigation Buttons */}
         <Button
           color="inherit"
           component={Link}
-          to="/"
-          startIcon={<HomeRoundedIcon />}
+          to="/products"
+          startIcon={<StorefrontIcon />}
         >
-          Home
+          Products
+        </Button>
+        <Button
+          color="inherit"
+          component={Link}
+          to="/signup"
+          startIcon={<PersonAddAlt1Icon />}
+        >
+          Sign Up
+        </Button>
+        <Button
+          color="inherit"
+          component={Link}
+          to="/signin"
+          startIcon={<LoginIcon />}
+        >
+          Sign In
         </Button>
       </Toolbar>
     </AppBar>
