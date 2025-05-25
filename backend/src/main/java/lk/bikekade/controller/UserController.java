48c9dadf4c7b365777db240e9fb83223f09f3522
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-05-25 16:20:40 +0530

    display info using AXIOS GET

--- backend/src/main/java/lk/bikekade/controller/UserController.java
+++ backend/src/main/java/lk/bikekade/controller/UserController.java
@@ -3,6 +3,7 @@ package lk.bikekade.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import lk.bikekade.model.User;
 import lk.bikekade.repository.UserRepository;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
    
 	@Autowired
