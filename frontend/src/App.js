99f4bf7539e2895a206d5ce849d14f8d6a5f7f22
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-05-25 15:46:43 +0530

    create homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './layout/Navbar';
+import Home from './pages/Home'
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Home></Home>
     </div>
   );
 }
