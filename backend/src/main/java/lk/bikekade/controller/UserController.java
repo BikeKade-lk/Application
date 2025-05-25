bcd77dc390bfc5e8ddcf3a3df7dc7a722b0a1f89
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-05-25 13:20:25 +0530

    getting data into database

--- backend/src/main/java/lk/bikekade/controller/UserController.java
+++ backend/src/main/java/lk/bikekade/controller/UserController.java
@@ -1,6 +1,9 @@
 package lk.bikekade.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +22,8 @@ public class UserController {
 		return userRepository.save(newUser);
 	}
 	
-	
+	@GetMapping("/users")
+	List<User> setAllUsers(){
+		return userRepository.findAll();
+	}
 }
