b93c195956cccd86432786f5ff9e948bb8ef0579
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-05-31 16:11:47 +0530

    configure routing using REACT-ROUTER-DOM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,21 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './layout/Navbar';
-import Home from './pages/Home'
+import Home from './pages/Home';
+import AddUser from './users/AddUser'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Home></Home>
+      <Router>
+          <Navbar></Navbar>
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/adduser" element={<AddUser/>}/>
+          </Routes>
+        
+      </Router>
+      
     </div>
   );
 }
