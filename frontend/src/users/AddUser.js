50c0e96e40fbebfbdcd21f1a0f44e338b70752ba
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-06-20 15:10:58 +0530

    Post data into database

--- frontend/src/users/AddUser.js
+++ frontend/src/users/AddUser.js
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function AddUser() {
-
+    let navigate=useNavigate();
     const [user,setUser]=useState({
            name:"",
            username:"",
@@ -12,12 +14,22 @@ export default function AddUser() {
 
     const onInputChange=(e)=>{
           setUser({...user,[e.target.name]:e.target.value})
+    };
+
+    const onSubmit=async(e)=>{
+        e.preventDefault();
+         await axios.post("http://localhost:8080/user",user)
+         navigate("/");
     }
   return (
     <div className="container">
         <div className="row">
             <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow ">
                 <h2 className="text-center m-4">Register User</h2>
+
+
+                <form onSubmit={(e)=>onSubmit(e)}>
+                
                 <div className="mb-3">
                     <label htmlFor="Name" className="form-label">
                         Name
@@ -40,8 +52,8 @@ export default function AddUser() {
                 </div>
                 <button type="submit" className="btn btn-outline-primary"> Submit </button>
 
-                <button type="reset" className="btn btn-outline-danger"> Cancel </button>
-
+                <button type="reset" className="btn btn-outline-danger mx-2"> Cancel </button>
+              </form>
             </div>
         </div>
     </div>
