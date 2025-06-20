7c2ef450d663dd16cefc1f8ae6d4dafefa623e82
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-06-20 20:16:58 +0530

    update backend

--- backend/src/main/java/lk/bikekade/controller/UserController.java
+++ backend/src/main/java/lk/bikekade/controller/UserController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lk.bikekade.exception.UserNotFoundException;
 import lk.bikekade.model.User;
 import lk.bikekade.repository.UserRepository;
 
@@ -27,5 +29,11 @@ public class UserController {
 	@GetMapping("/users")
 	List<User> setAllUsers(){
 		return userRepository.findAll();
+	}
+	
+	@GetMapping(".user/{id}")
+	User getUserById(@PathVariable Long id) {
+		return userRepository.findById(id)
+				.orElseThrow(()->new UserNotFoundException(id));
 	}
 }
