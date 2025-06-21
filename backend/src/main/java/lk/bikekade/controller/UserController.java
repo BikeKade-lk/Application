fb12bb400092e4ca81fd91a51a784a7ba194ed96
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-06-21 09:33:04 +0530

    Delete the user of specific id

--- backend/src/main/java/lk/bikekade/controller/UserController.java
+++ backend/src/main/java/lk/bikekade/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,7 +41,7 @@ public class UserController {
 	
 	@PutMapping("/user/{id}")
 	User updateUser(@RequestBody User newUser,@PathVariable Long id) {
-		return userRepository.findById(id)
+		return userRepository.findById(id) 
 		.map(user ->{
 			user.setUsername(newUser.getUsername());
 			user.setName(newUser.getName());
@@ -48,5 +49,13 @@ public class UserController {
 			return userRepository.save(user);
 			
 		}).orElseThrow(()->new UserNotFoundException(id));
+	}
+	@DeleteMapping("/user/{id}")
+	String deleteUser(@PathVariable Long id) {
+		if(!userRepository.existsById(id)) {
+			userRepository.deleteById(id);
+		}
+		userRepository.deleteById(id);
+		return "User with id "+id+" has been deleter success.";
 	}
 }
