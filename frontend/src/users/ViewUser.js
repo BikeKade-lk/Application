ca57ad40ca755e818b13fbebe45482bab30faa2d
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-06-21 11:21:47 +0530

    update View Information of specific User

--- frontend/src/users/ViewUser.js
+++ frontend/src/users/ViewUser.js
@@ -1,7 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link ,useParams} from "react-router-dom";
+import { useEffect,useState } from "react";
 
 export default function ViewUser() {
+    const [user,setUser]=useState({
+        name:"",
+        username:"",
+        email:""
+    })
+    const{id}=useParams();
+
+    useEffect(()=>{
+       loadUser();
+    },[])
+
+    const loadUser=async()=>{
+        const result=await axios.get(`http://localhost:8080/user/${id}`)
+        setUser(result.data)
+    }
   return (
     <div className="container">
       <div className="row">
@@ -10,16 +27,19 @@ export default function ViewUser() {
 
           <div className="card">
             <div className="card-header">
-                Details of user id:
+                Details of user id:{user.id}
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item">
                         <b>Name:</b>
+                        {user.name}
                     </li>
                     <li className="list-group-item">
                         <b>Username:</b>
+                        {user.username}
                     </li>
                     <li className="list-group-item">
                         <b>Email:</b>
+                        {user.email}
                     </li>
                 </ul>
             </div>
