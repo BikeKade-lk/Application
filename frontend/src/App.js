1d0b9af39f5cca6f8563a0ed8cbe84b3c17a9ad4
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2025-06-21 09:55:25 +0530

    Edit user using AXIOS PUT - REACT|

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
-import AddUser from './users/AddUser'
+import AddUser from './users/AddUser';
+import EditUser from './users/EditUser';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home/>}/>
             <Route exact path="/adduser" element={<AddUser/>}/>
+            <Route exact path="/edituser/:id" element={<EditUser/>}/>
           </Routes>
         
       </Router>
